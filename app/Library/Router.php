21b73e6c1dfc871f51fca715cff0d2aa7103e584
Mr. Protect Codes <codevault@ungeeksi.net>
2024-08-24 23:05:20 -0300

    Initial test

---
+++ app/Library/Router.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace RandomDatasWithPhp\Library;
+
+class Router
+{
+    public function run()
+    {
+        require '../app/Views/index.php';
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+use RandomDatasWithPhp\Library\Router;
+
+ini_set('display_errors', 1);
+
+require '../vendor/autoload.php';
+
+(new Router)->run();
